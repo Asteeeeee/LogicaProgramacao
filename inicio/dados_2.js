fdd58aaf8f71da6ef1ad47a6101128f9d31be3f1
Guilherme Rocha <111608183+Asteeeeee@users.noreply.github.com>
2023-09-29 19:50:05 -0300

    Add files via upload

---
+++ inicio/dados_2.js
@@ -0,0 +1,16 @@
+console.log(19.9 * 0.6);
+
+let preco = 19.90;
+let desconto = 0.5;
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+let nome = "Caderno"; // String (Texto) -> sequencia de simbolos
+let categoria = "Papelaria";
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria
+    + ", Preco: " + preco
+    + ", Desconto: " + desconto);
+
+console.log(1 + 1); // Operacao
+console.log("1" + 1); //Concatenacao 
