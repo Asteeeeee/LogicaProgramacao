8d6c1317c3b7a8e2ae49da5b34ea5806a6ddf184
Guilherme Rocha <111608183+Asteeeeee@users.noreply.github.com>
2023-10-10 23:42:56 -0300

    Add files via upload

---
+++ inicio/desafio_area.js
@@ -0,0 +1,7 @@
+// PI * raio * raio
+
+let raio = 10;
+const PI = Math.PI;
+const area = PI * raio * raio;
+
+console.log("O resultado da area e " + area + "m2.")
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,11 @@
+let a = 7;
+let b = 94;
+let c;
+
+c = a;
+a = b; 
+b = c
+
+// depois da troca... a = 94 e b = 7
+console.log(a); // 94
+console.log(b); // 7
