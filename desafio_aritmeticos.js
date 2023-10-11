dc75336e82b75e255913bbf647a08434da8830a8
Guilherme Rocha <111608183+Asteeeeee@users.noreply.github.com>
2023-10-10 23:43:44 -0300

    Add files via upload

---
+++ desafio_aritmeticos.js
@@ -0,0 +1,4 @@
+let f = 72;
+let c = (5 * (f - 32)) / 9;
+
+console.log("A temperatura convertida fica igual a " + c + " Celsius.");
---
+++ unarios_2.js
@@ -0,0 +1,9 @@
+let a = 1;
+let b = 2;
+
+//              1   2   3  Ordem de Prioridade
+// console.log(++a === b++);
+
+a++;
+console.log(a === b);
+b++; 
