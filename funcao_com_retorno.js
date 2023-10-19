5b10ed5324b3bfc82700f090b705936d1f72c7c3
Guilherme Rocha <111608183+Asteeeeee@users.noreply.github.com>
2023-10-18 21:05:24 -0300

    Função

---
+++ funcao_com_retorno.js
@@ -0,0 +1,14 @@
+function sempreRetornaUm() {
+    return 1;
+}
+
+function textoOuNumero(returnaTexto){
+    if(retornaTexto){
+        return "Sou um texto";
+    } else {
+        return 123;
+    }
+}
+
+let valor = sempreRetornaUm();
+console.log(valor);
