c0a8a080a2d981fd398e04621bbfebcc9697673e
Guilherme Rocha <111608183+Asteeeeee@users.noreply.github.com>
2023-11-06 20:03:12 -0300

    Add files via upload

---
+++ arrays/basico_2.js
@@ -0,0 +1,11 @@
+const nomes = [];
+console.log(typeof nomes);
+
+nomes[0] = "Pedro";
+nomes[1] = "Ana";
+nomes[2] = "Bia";
+nomes[3] = "Gui";
+nomes[4] = "Rebeca";
+nomes[10] = "Thiago";
+
+console.log(nomes);
